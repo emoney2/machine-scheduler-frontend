76795012b221add113ab2d6740f199004ce4c6e5
Justin Eckard <eckardjustin@gmail.com>
2025-05-20 11:48:32 -0400

    Initial commit: scheduler + order-submission

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
+// src/index.js
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';      // ← import
 import App from './App';
 
-const root = createRoot(document.getElementById('root'));
-root.render(<App />);
+ReactDOM.render(
+  <BrowserRouter>                                    // ← wrap here
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root')
+);
