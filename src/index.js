34cb29bda61c7459d543bd5cf084500f224fb8b4
Justin Eckard <eckardjustin@gmail.com>
2025-05-23 20:00:54 -0400

    Remove stray wrap‐here comment

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';      // ← import
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 ReactDOM.render(
