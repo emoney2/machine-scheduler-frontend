cafd8db98b9254b72e554adaba81d9a23adf2f22
Justin Eckard <eckardjustin@gmail.com>
2025-08-17 22:43:18 -0400

    Speed up thumbnails: cached Drive meta + session reuse + smaller sz

--- src/App.js
+++ src/App.js
@@ -439,7 +439,7 @@ function toPreviewUrl(originalUrl) {
     return '';
   }
   // bump size to w=512 for a sharper card preview
-  return `${API_ROOT}/drive/proxy/${id}?thumb=1&sz=w240`;
+  return `${API_ROOT}/drive/proxy/${id}?thumb=1&sz=w200`;
 }
 
 // Full-view URL (original quality / inline display)
